c6487907e2fd7ee3422240bd417c15876e409752
Ian Hooyboer <ianhooyboer@u.boisestate.edu>
2019-11-02 12:25:57 -0600

    removed debug printf

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,6 @@
 <?php
 
-include ($_SERVER["DOCUMENT_ROOT"] . "/includes/arrays.php");
- 
-echo $_SERVER["DOCUMENT_ROOT"] . "/includes/pages/dashboard.php";
+include ($_SERVER["DOCUMENT_ROOT"] . "/includes/arrays.php"); 
 
 ?>
 
